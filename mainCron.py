6c2049b90d9d63eda4916db599b99b7c856a314f
wikipedia555 <kentbrokmen55@gmail.com>
2020-04-01 13:27:07 +0600

    add class

--- mainCron.py
+++ mainCron.py
@@ -1,105 +1,116 @@
 import datetime
-# weekday (0 = Monday)
-# min. hour. day. weekday
-cronString = "15 15 31 7"
-nowTime = datetime.datetime.now().timestamp()
-#check = (datetime.datetime.fromtimestamp(nowTime).day)
-dayOfMonth = [31,28,31,30,31,30,31,31,30,31,30,31]
-nowTime = datetime.datetime.now().replace(microsecond=0).replace(second=0)
-timerTime = nowTime.timestamp()
-#print(int(nowTime.timestamp()))
-#print(nowTime)
-cronStringLst = cronString.split()
-#print(cronStringLst)
-nowTimeLst = [nowTime.minute, nowTime.hour, nowTime.day, nowTime.month, nowTime.year, nowTime.weekday()]
-print(nowTimeLst)
-def calcDayForWeekday(timerTime, dayTimer):
-    while int(datetime.datetime.fromtimestamp(timerTime).day) != int(dayTimer):
-        timerTime = timerTime + 7 * 86400
 
-    return timerTime
+class Cron:
+
+    def __init__(self, cronString):
+
+        # weekday (0 = Monday)
+        # min. hour. day. weekday
+        #cronString = "15 15 31 7"
+
+        #check = (datetime.datetime.fromtimestamp(nowTime).day)
+        self.dayOfMonth = [31,28,31,30,31,30,31,31,30,31,30,31]
+        #self.nowTime = datetime.datetime.now().replace(microsecond=0).replace(second=0)
+        self.cronStringLst = cronString.split()
+        #print(nowTimeLst)
 
 
-def calcDayForMonth (timerTime, day):
-    if int(dayOfMonth[datetime.datetime.fromtimestamp(timerTime).month - 1]) > int(day):
-        pass
-    else:
-        today = datetime.datetime.fromtimestamp(timerTime).day
-        timerTime = timerTime + (int(dayOfMonth[datetime.datetime.fromtimestamp(timerTime).month - 1]) - today + 1) * 86400
-        while int(dayOfMonth[datetime.datetime.fromtimestamp(timerTime).month - 1]) < int(day):
-            timerTime = timerTime + int(dayOfMonth[datetime.datetime.fromtimestamp(timerTime).month - 1]) * 86400
+    def calcDayForWeekday(self, timerTime, dayTimer):
+        while int(datetime.datetime.fromtimestamp(timerTime).day) != int(dayTimer):
+            timerTime = timerTime + 7 * 86400
 
-    return timerTime
+        return timerTime
 
 
-for i in range(len(cronStringLst)):
-    if i == 0:
-        if cronStringLst[i] == "*":
-            timerTime = timerTime + 60
-        else:
-            if int(nowTimeLst[i]) < int(cronStringLst[i]):
-                timerTime = timerTime + (int(cronStringLst[i]) - int(nowTimeLst[i]))*60
-            elif int(nowTimeLst[i]) > int(cronStringLst[i]):
-                timerTime = timerTime + (60-int(nowTimeLst[i]) + int(cronStringLst[i])) * 60
-            else:
-                timerTime = timerTime + 3600
-    elif i == 1:
-        if cronStringLst[i] == "*":
+    def calcDayForMonth (self, timerTime, day):
+        if int(self.dayOfMonth[datetime.datetime.fromtimestamp(timerTime).month - 1]) > int(day):
             pass
         else:
-            nowHour = datetime.datetime.fromtimestamp(timerTime).hour
-            if int(nowHour) < int(cronStringLst[i]):
-                timerTime = timerTime + (int(cronStringLst[i]) - nowHour) * 3600
-            elif int(nowHour) > int(cronStringLst[i]):
-                timerTime = timerTime + (24-nowHour + int(cronStringLst[i]))*3600
-            else:
-                pass
-    elif i == 2:
-        if(cronStringLst[i] == '*'):
-            pass
-        else:
-            nowDay = datetime.datetime.fromtimestamp(timerTime).day
-            if nowDay < int(cronStringLst[i]):
-                timerTime = calcDayForMonth(timerTime, cronStringLst[i])
-                timerTime = timerTime + (int(cronStringLst[i]) - nowDay) * 86400
-            elif nowDay > int(cronStringLst[i]):
-                timerTime = calcDayForMonth(timerTime, cronStringLst[i])
-                #timerTime = timerTime + (int(dayOfMount[datetime.datetime.fromtimestamp(timerTime).month - 1]) - int(cronStringLst[i]) + nowDay) * 86400
-                numberOfDays = dayOfMonth[datetime.datetime.fromtimestamp(timerTime).month - 1]
-                if datetime.datetime.fromtimestamp(timerTime).month == 2:
-                    if datetime.datetime.fromtimestamp(timerTime).year % 4 == 0:
-                        numberOfDays = numberOfDays + 1
-                timerTime = timerTime + (numberOfDays - nowDay + int(cronStringLst[i])) * 86400
-            else:
-                pass
-    elif i == 3:
-        if(cronStringLst[i] == "*"):
-            pass
-        else:
-            if(cronStringLst[i-1] == "*"):
-                numberOfWeekday = datetime.datetime.fromtimestamp(timerTime).weekday() + 1
-                if numberOfWeekday < int(cronStringLst[i]):
-                    timerTime = timerTime + (int(cronStringLst[i]) - numberOfWeekday) * 86400
-                elif numberOfWeekday > int(cronStringLst[i]):
-                    timerTime = timerTime + (7 - numberOfWeekday + int(cronStringLst[i])) * 86400
+            today = datetime.datetime.fromtimestamp(timerTime).day
+            timerTime = timerTime + (int(self.dayOfMonth[datetime.datetime.fromtimestamp(timerTime).month - 1]) - today + 1) * 86400
+            while int(self.dayOfMonth[datetime.datetime.fromtimestamp(timerTime).month - 1]) < int(day):
+                timerTime = timerTime + int(self.dayOfMonth[datetime.datetime.fromtimestamp(timerTime).month - 1]) * 86400
+
+        return timerTime
+
+    def calcCron(self, nowTime):
+        timerTime = nowTime.timestamp()
+        nowTimeLst = [nowTime.minute, nowTime.hour, nowTime.day, nowTime.month, nowTime.year, nowTime.weekday()]
+        for i in range(len(self.cronStringLst)):
+            if i == 0:
+                if self.cronStringLst[i] == "*":
+                    timerTime = timerTime + 60
                 else:
+                    if int(nowTimeLst[i]) < int(self.cronStringLst[i]):
+                        timerTime = timerTime + (int(self.cronStringLst[i]) - int(nowTimeLst[i]))*60
+                    elif int(nowTimeLst[i]) > int(self.cronStringLst[i]):
+                        timerTime = timerTime + (60-int(nowTimeLst[i]) + int(self.cronStringLst[i])) * 60
+                    else:
+                        timerTime = timerTime + 3600
+            elif i == 1:
+                if self.cronStringLst[i] == "*":
                     pass
-            else:
-                dayTimer = int(cronStringLst[i-1])
-                numberOfWeekday = datetime.datetime.fromtimestamp(timerTime).weekday() + 1
-                if numberOfWeekday < int(cronStringLst[i]):
-                    timerTime = timerTime + (int(cronStringLst[i]) - numberOfWeekday) * 86400
-                    timerTime = calcDayForWeekday(timerTime, cronStringLst[i-1])
-                elif numberOfWeekday > int(cronStringLst[i]):
-                    timerTime = timerTime + (7 - numberOfWeekday + int(cronStringLst[i])) * 86400
-                    timerTime = calcDayForWeekday(timerTime, cronStringLst[i-1])
                 else:
-                    timerTime = calcDayForWeekday(timerTime, cronStringLst[i-1])
+                    nowHour = datetime.datetime.fromtimestamp(timerTime).hour
+                    if int(nowHour) < int(self.cronStringLst[i]):
+                        timerTime = timerTime + (int(self.cronStringLst[i]) - nowHour) * 3600
+                    elif int(nowHour) > int(self.cronStringLst[i]):
+                        timerTime = timerTime + (24-nowHour + int(self.cronStringLst[i]))*3600
+                    else:
+                        pass
+            elif i == 2:
+                if(self.cronStringLst[i] == '*'):
+                    pass
+                else:
+                    nowDay = datetime.datetime.fromtimestamp(timerTime).day
+                    if nowDay < int(self.cronStringLst[i]):
+                        timerTime = self.calcDayForMonth(timerTime, self.cronStringLst[i])
+                        timerTime = timerTime + (int(self.cronStringLst[i]) - nowDay) * 86400
+                    elif nowDay > int(self.cronStringLst[i]):
+                        timerTime = self.calcDayForMonth(timerTime, self.cronStringLst[i])
+                        #timerTime = timerTime + (int(dayOfMount[datetime.datetime.fromtimestamp(timerTime).month - 1]) - int(cronStringLst[i]) + nowDay) * 86400
+                        numberOfDays = self.dayOfMonth[datetime.datetime.fromtimestamp(timerTime).month - 1]
+                        if datetime.datetime.fromtimestamp(timerTime).month == 2:
+                            if datetime.datetime.fromtimestamp(timerTime).year % 4 == 0:
+                                numberOfDays = numberOfDays + 1
+                        timerTime = timerTime + (numberOfDays - nowDay + int(self.cronStringLst[i])) * 86400
+                    else:
+                        pass
+            elif i == 3:
+                if(self.cronStringLst[i] == "*"):
+                    pass
+                else:
+                    if(self.cronStringLst[i-1] == "*"):
+                        numberOfWeekday = datetime.datetime.fromtimestamp(timerTime).weekday() + 1
+                        if numberOfWeekday < int(self.cronStringLst[i]):
+                            timerTime = timerTime + (int(self.cronStringLst[i]) - numberOfWeekday) * 86400
+                        elif numberOfWeekday > int(self.cronStringLst[i]):
+                            timerTime = timerTime + (7 - numberOfWeekday + int(self.cronStringLst[i])) * 86400
+                        else:
+                            pass
+                    else:
+                        dayTimer = int(self.cronStringLst[i-1])
+                        numberOfWeekday = datetime.datetime.fromtimestamp(timerTime).weekday() + 1
+                        if numberOfWeekday < int(self.cronStringLst[i]):
+                            timerTime = timerTime + (int(self.cronStringLst[i]) - numberOfWeekday) * 86400
+                            timerTime = self.calcDayForWeekday(timerTime, self.cronStringLst[i-1])
+                        elif numberOfWeekday > int(self.cronStringLst[i]):
+                            timerTime = timerTime + (7 - numberOfWeekday + int(self.cronStringLst[i])) * 86400
+                            timerTime = self.calcDayForWeekday(timerTime, self.cronStringLst[i-1])
+                        else:
+                            timerTime = self.calcDayForWeekday(timerTime, self.cronStringLst[i-1])
 
 
 
 
-print(datetime.datetime.fromtimestamp(timerTime).isoformat())
+        print(datetime.datetime.fromtimestamp(timerTime).isoformat())
+        return timerTime
+
+if __name__ == "__main__":
+    cronStr = "* 13 8 *"
+    cron = Cron(cronStr)
+    nowTime = datetime.datetime.now().replace(microsecond=0).replace(second=0)
+    cron.calcCron(nowTime)
 
 # print(nowTime.second)
 # print(nowTime.minute)
